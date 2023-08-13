0589e489071d416af8b8f8071849b2d1b8dff72c
Gil Gottlieb <gilgottlieb1@gmail.com>
2023-08-13 20:10:23 +0300

    - Created the words API

--- frontend-v2/src/components/UploadSong.tsx
+++ frontend-v2/src/components/UploadSong.tsx
@@ -2,38 +2,39 @@
 import React, {useState} from 'react';
 
 function UploadSong() {
-    const [file, setFile] = useState<File | null>(null);
+    const [files, setFiles] = useState<FileList>([]);
     const [songName, setSongName] = useState<String>("");
     const [artistName, setArtistName] = useState<String>("");
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const uploadedFile = event.target.files ? event.target.files[0] : null;
-        setFile(uploadedFile);
+        const uploadedFiles = event.target.files || [];
+        setFiles(uploadedFiles);
     };
 
+    const _doUpload = async (file) => {
+        const fileContent = await file.text();
+        await fetch("http://localhost:8000/api/songs/add", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                'cache-control': 'no-cache'
+            },
+            body: JSON.stringify({
+                "song_name": songName,
+                "artist_name": artistName,
+                "content": fileContent,
+                "file_name": file.name
+            })
+        });
+    }
+
     const handleUpload = async () => {
-        if (file) {
-            const fileContent = await file.text()
-
+        for (const file of files) {
             try {
-                const response = await fetch("http://localhost:8000/api/songs/add", {
-                    method: 'POST',
-                    headers: {
-                        "Content-Type": "application/json",
-                        'cache-control': 'no-cache'
-                    },
-                    body: JSON.stringify({
-                        "song_name": songName,
-                        "artist_name": artistName,
-                        "content": fileContent,
-                        "file_name": file.name
-                    })
-                });
-
-                const data = await response.json();
-                console.log(data);
-            } catch (error) {
-                console.error("Error uploading file:", error);
+                await _doUpload(file);
+            }
+            catch (e) {
+                console.log('Failed uploading');
             }
         }
     };
@@ -42,7 +43,7 @@ function UploadSong() {
         <div className={"flex flex-col gap-2"}>
             <div className={"text-xl"}>Upload new song</div>
             <div className={"flex flex-col gap-4"}>
-                <input type="file" onChange={handleFileChange}/>
+                <input type="file" onChange={handleFileChange} multiple/>
                 <input type="text" placeholder={"Artist name"} onChange={(e) => setArtistName(e.target.value)}/>
                 <input type="text" placeholder={"Song name"} onChange={(e) => setSongName(e.target.value)}/>
                 <button onClick={handleUpload}>Upload</button>
